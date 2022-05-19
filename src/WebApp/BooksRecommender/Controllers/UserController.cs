d0a046ddbe6a134c8077aebfe79ebdac8c0a8343
Evva28 <93273156+Evva28@users.noreply.github.com>
2022-05-19 13:22:44 +0200

    nowy branch, nowy controller

---
+++ src/WebApp/BooksRecommender/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using BooksRecommender.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksRecommender.Controllers
+{
+    [ApiController]
+    public class UserController : Controller
+    {
+        private IUserSignInManager signInManager;
+        private IDatabase dbManager;
+        public UserController(IUserSignInManager signmg, IDatabase db)
+        {
+            signInManager = signmg;
+            dbManager = db;
+        }
+
+        public void ShowBooks()
+        {
+
+        }
+        public void SetRating()
+        {
+
+        }
+        public void SetAsRead()
+        {
+
+        }
+        public void GetRecommendationBasedOnFavorites()
+        {
+
+        }
+        public void GetRecommendationBasedOnAverage()
+        {
+
+        }
+        public void GetRecommendationBasedOnBook()
+        {
+
+        }
+        public void ShowReadBooks()
+        {
+
+        }
+    }
+}
